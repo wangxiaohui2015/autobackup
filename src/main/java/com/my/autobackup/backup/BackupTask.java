9d5a12ee2396f86600e93dc065188cefe20669b2
sunny wang <test@test.com>
2016-08-12 18:04:03 +0800

    Change logs and default configurations.

--- src/main/java/com/my/autobackup/backup/BackupTask.java
+++ src/main/java/com/my/autobackup/backup/BackupTask.java
@@ -16,106 +16,101 @@ import com.my.autobackup.common.FileUtil;
  */
 public class BackupTask extends TimerTask {
 
-	private static Logger logger = Logger.getLogger(BackupTask.class);
+    private static Logger logger = Logger.getLogger(BackupTask.class);
 
-	@Override
-	public void run() {
-		try {
-			logger.info("Begin to execute backup task.");
-			BackupConfigPropertiesUtil.getInstance().reloadProperties();
-			BackupTaskController controller = new BackupTaskController();
-			executeBackupTask(controller);
+    @Override
+    public void run() {
+        try {
+            logger.info("Begin to execute backup task.");
+            BackupConfigPropertiesUtil.getInstance().reloadProperties();
+            BackupTaskController controller = new BackupTaskController();
+            executeBackupTask(controller);
 
-			// Waiting for all tasks to finish.
-			controller.await();
-			logger.info("End to execute backup task.");
-		} catch (Throwable e) {
-			logger.error("Exception occurred while executing backup task.", e);
-		}
-	}
+            // Waiting for all tasks to finish.
+            controller.await();
+            logger.info("End to execute backup task.");
+        } catch (Throwable e) {
+            logger.error("Exception occurred while executing backup task.", e);
+        }
+    }
 
-	private void executeBackupTask(BackupTaskController controller) {
-		List<String> sourceKeys = BackupConfigPropertiesUtil.getInstance()
-				.getAllSourceKeys();
-		for (String sourceKey : sourceKeys) {
-			String destKey = BackupConfigPropertiesUtil.getInstance()
-					.getDestKeyBySourceKey(sourceKey);
-			String sourceDir = BackupConfigPropertiesUtil.getInstance()
-					.getValue(sourceKey);
-			String destDir = BackupConfigPropertiesUtil.getInstance().getValue(
-					destKey);
-			if (null == sourceDir || "".equals(sourceDir)) {
-				logger.warn("sourceDir is null or empty, sourceDir: "
-						+ sourceDir + ", destDir: " + destDir);
-				continue;
-			}
+    private void executeBackupTask(BackupTaskController controller) {
+        List<String> sourceKeys = BackupConfigPropertiesUtil.getInstance().getAllSourceKeys();
+        for (String sourceKey : sourceKeys) {
+            String destKey = BackupConfigPropertiesUtil.getInstance()
+                            .getDestKeyBySourceKey(sourceKey);
+            String sourceDir = BackupConfigPropertiesUtil.getInstance().getValue(sourceKey);
+            String destDir = BackupConfigPropertiesUtil.getInstance().getValue(destKey);
+            if (null == sourceDir || "".equals(sourceDir)) {
+                logger.warn("sourceDir is null or empty, sourceDir: " + sourceDir + ", destDir: "
+                                + destDir);
+                continue;
+            }
 
-			if (null == destDir || "".equals(destDir)) {
-				logger.warn("destDir is null or empty, sourceDir: " + sourceDir
-						+ ", destDir: " + destDir);
-				continue;
-			}
+            if (null == destDir || "".equals(destDir)) {
+                logger.warn("destDir is null or empty, sourceDir: " + sourceDir + ", destDir: "
+                                + destDir);
+                continue;
+            }
 
-			File sourceFile = new File(sourceDir);
-			File destFile = new File(BackupConfigPropertiesUtil.getInstance()
-					.getValue(destKey));
+            File sourceFile = new File(sourceDir);
+            File destFile = new File(BackupConfigPropertiesUtil.getInstance().getValue(destKey));
 
-			if (FileUtil.isSubFile(sourceFile, destFile)) {
-				logger.warn("Source dir is the sub dir of dest dir, or dest dir is the sub dir of source dir, skip this entry, source dir: "
-						+ sourceDir + ", dest dir: " + destDir);
-				continue;
-			}
+            if (FileUtil.isSubFile(sourceFile, destFile)) {
+                logger.warn("Source dir is the sub dir of dest dir, or dest dir is the sub dir of source dir, skip this entry, source dir: "
+                                + sourceDir + ", dest dir: " + destDir);
+                continue;
+            }
 
-			logger.info("Processing backup task, sourceDir: " + sourceDir
-					+ ", destDir: " + destDir);
+            logger.info("Processing backup task, sourceDir: " + sourceDir + ", destDir: "
+                            + destDir);
 
-			if (!sourceFile.isDirectory() || !sourceFile.exists()) {
-				logger.warn("Source dir isn't a directory or doesn't exist, sourceDir: "
-						+ sourceFile);
-				continue;
-			}
+            if (!sourceFile.isDirectory() || !sourceFile.exists()) {
+                logger.warn("Source dir isn't a directory or doesn't exist, sourceDir: "
+                                + sourceFile);
+                continue;
+            }
 
-			if (destFile.exists()) {
-				if (destFile.isFile()) {
-					logger.warn("Dest dir exists, but it isn't a directory.");
-					continue;
-				}
-			}
+            if (destFile.exists()) {
+                if (destFile.isFile()) {
+                    logger.warn("Dest dir exists, but it isn't a directory.");
+                    continue;
+                }
+            }
 
-			// Append source file name to dest file name
-			destDir = destDir + File.separator + sourceFile.getName();
+            // Append source file name to dest file name
+            destDir = destDir + File.separator + sourceFile.getName();
 
-			if (!destFile.exists()) {
-				logger.info("Dest dir doesn't exist, create it now.");
-				destFile.mkdirs();
-			}
-			processBackupTask(sourceDir, destDir, controller);
-		}
-	}
+            if (!destFile.exists()) {
+                logger.info("Dest dir doesn't exist, create it now.");
+                destFile.mkdirs();
+            }
+            processBackupTask(sourceDir, destDir, controller);
+        }
+    }
 
-	private void processBackupTask(String sourceDir, String destDir,
-			BackupTaskController controller) {
-		File sourceFile = new File(sourceDir);
-		File[] files = sourceFile.listFiles();
-		for (File file : files) {
-			File newDestFile = new File(destDir + File.separator
-					+ file.getName());
-			if (file.isFile()) {
-				if (!newDestFile.exists()) {
-					FileEncryptionExecutor.getInstance().submitTask(
-							new FileEncryptionThread(file.getAbsolutePath(),
-									newDestFile.getAbsolutePath(), controller),
-							controller);
-				} else {
-					// ... Check if this file is changed or not.
-				}
-			} else {
-				if (!newDestFile.exists()) {
-					newDestFile.mkdir();
-				}
-				processBackupTask(file.getAbsolutePath(),
-						newDestFile.getAbsolutePath(), controller);
-			}
-		}
-	}
+    private void processBackupTask(String sourceDir, String destDir,
+                    BackupTaskController controller) {
+        File sourceFile = new File(sourceDir);
+        File[] files = sourceFile.listFiles();
+        for (File file : files) {
+            File newDestFile = new File(destDir + File.separator + file.getName());
+            if (file.isFile()) {
+                if (!newDestFile.exists()) {
+                    FileEncryptionExecutor.getInstance()
+                                    .submitTask(new FileEncryptionThread(file.getAbsolutePath(),
+                                                    newDestFile.getAbsolutePath(), controller),
+                                    controller);
+                } else {
+                    // ... Check if this file is changed or not.
+                }
+            } else {
+                if (!newDestFile.exists()) {
+                    newDestFile.mkdir();
+                }
+                processBackupTask(file.getAbsolutePath(), newDestFile.getAbsolutePath(),
+                                controller);
+            }
+        }
+    }
 }
--- src/main/java/com/my/autobackup/backup/FileEncryptionThread.java
+++ src/main/java/com/my/autobackup/backup/FileEncryptionThread.java
@@ -11,40 +11,35 @@ import com.my.autobackup.backup.util.ServiceConfigPropertiesUtil;
  */
 public class FileEncryptionThread implements Runnable {
 
-	private static Logger logger = Logger.getLogger(FileEncryptionThread.class);
+    private static Logger logger = Logger.getLogger(FileEncryptionThread.class);
 
-	private String sourceFile;
-	private String destFile;
-	private BackupTaskController controller;
+    private String sourceFile;
+    private String destFile;
+    private BackupTaskController controller;
 
-	/**
-	 * Constructor method.
-	 * 
-	 * @param sourceFile
-	 *            source file
-	 * @param destFile
-	 *            destination file
-	 * @param controller
-	 *            BackupTaskController object
-	 */
-	public FileEncryptionThread(String sourceFile, String destFile,
-			BackupTaskController controller) {
-		this.sourceFile = sourceFile;
-		this.destFile = destFile;
-		this.controller = controller;
-	}
+    /**
+     * Constructor method.
+     * 
+     * @param sourceFile source file
+     * @param destFile destination file
+     * @param controller BackupTaskController object
+     */
+    public FileEncryptionThread(String sourceFile, String destFile,
+                    BackupTaskController controller) {
+        this.sourceFile = sourceFile;
+        this.destFile = destFile;
+        this.controller = controller;
+    }
 
-	public void run() {
-		try {
-			FileEncryptor.encryptFile(ServiceConfigPropertiesUtil.getInstance()
-					.getBackupKey(), sourceFile, destFile);
-			logger.info("Backup file successfully, sourceFile: " + sourceFile
-					+ ", destFile: " + destFile);
-		} catch (Exception e) {
-			logger.error("Backup file failed, sourceFile: " + sourceFile
-					+ ", destFile: " + destFile, e);
-		} finally {
-			controller.finishTask();
-		}
-	}
+    public void run() {
+        try {
+            FileEncryptor.encryptFile(ServiceConfigPropertiesUtil.getInstance().getBackupKey(),
+                            sourceFile, destFile);
+            logger.info("Backup succeed, source: " + sourceFile + ", dest: " + destFile);
+        } catch (Exception e) {
+            logger.error("Backup failed, source: " + sourceFile + ", source: " + destFile, e);
+        } finally {
+            controller.finishTask();
+        }
+    }
 }
