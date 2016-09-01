43286e92ef197f4f0b7174fdb8193093bc0f4a9d
sunny wang <test@test.com>
2016-09-01 18:37:47 +0800

    Add JRE and refactory install and restore scripts.

--- build/scripts/install_service.bat
+++ build/scripts/install_service.bat
@@ -2,22 +2,13 @@
 
 echo=
 
-if not "%JAVA_HOME%" == "" goto gotJdkHome
-echo Please set JAVA_HOME
-goto end
-
-:gotJdkHome
-if not exist "%JAVA_HOME%\jre\bin\javaw.exe" goto noJavaHome
-goto install
-
-:noJavaHome
-echo Cannot find javaw.exe in JAVA_HOME
-goto end
-
 :install
 cd %~dp0 %root%
+cd ..\
+set BASE_PATH="%cd%"
+cd bin
 set SERVICE_DESC=This is the Autobakup service, you can backup anything on your computer.
-JavaService.exe -install "Autobackup" "%JAVA_HOME%/jre/bin/server/jvm.dll" -Xmx1024m -Djava.class.path=../lib/autobackup-backup-0.0.0-RELEASE.jar -DrootDir="%CD%/.." -start com.my.autobackup.backup.ServiceMain -method startService -stop com.my.autobackup.backup.ServiceMain -method stopService -out "%CD%/../logs/service_out.log" -err "%CD%/../logs/service_err.log" -current "%CD%" -auto -description "%SERVICE_DESC%"
+JavaService.exe -install "Autobackup" "%BASE_PATH%\jre%%JRE_VERSION%%\bin\server\jvm.dll" -Xmx1024m -Djava.class.path=%BASE_PATH%\lib\autobackup-backup-1.1.0-RELEASE.jar -DrootDir="%BASE_PATH%" -start com.my.autobackup.backup.ServiceMain -method startService -stop com.my.autobackup.backup.ServiceMain -method stopService -out "%BASE_PATH%\logs\service_out.log" -err "%BASE_PATH%\logs\service_err.log" -current "%CD%" -auto -description "%SERVICE_DESC%"
 
 :end
 
--- build/scripts/restore.bat
+++ build/scripts/restore.bat
@@ -1,4 +1,12 @@
 @echo off
-java -jar ../lib/autobackup-restore-0.0.0-RELEASE.jar
+
+echo=
+
+cd %~dp0 %root%
+cd ..\
+set BASE_PATH="%cd%"
+cd bin
+cd %BASE_PATH%\jre%%JRE_VERSION%%\bin
+java -jar %BASE_PATH%\lib\autobackup-restore-1.1.0-RELEASE.jar
 
 pause
